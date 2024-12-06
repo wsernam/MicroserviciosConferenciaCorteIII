d917f07a5c24547313e71194de839d100026eda7
Mónica Castellanos Méndez <monicacastellanos@unicauca.edu.co>
2024-12-05 20:48:38 -0500

    Se crean las carpetas, clases he interfaces necesarias para la arquitectura hexagonal

---
+++ conferencia/src/main/java/co/unicauca/edu/conferencia/ConferenciaAplicacion.java
@@ -0,0 +1,13 @@
+package co.unicauca.edu.conferencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenciaAplicacion {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenciaAplicacion.class, args);
+	}
+
+}
---
+++ conferencia/src/test/java/co/unicauca/edu/conferencia/ConferenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.unicauca.edu.conferencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
